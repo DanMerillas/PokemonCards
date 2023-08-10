451719c5b6719c7a30548ac4daaf19bcaa3460d8
Daniel Merillas <dmerillasg@minsait.com>
2023-08-10 21:33:11 +0200

    Filtros por series y fix de estilos

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,28 @@ import React, { useEffect } from 'react';
 import './App.css';
 import noImage from './images/no-image.png'
 
+interface ISeries {
+  name: string
+  id: string
+}
+
 
 function App() {
 
   const [cards, setCards] = React.useState<any>([])
+  const [series, setSeries] = React.useState<ISeries[]>([{ name: 'Todas', id: '' }])
   const [filterCards, setFilterCards] = React.useState<any>([])
   const [currentCard, setCurrentCard] = React.useState<any>(undefined)
   const [textSearch, setTextSearch] = React.useState<string>('')
   const [pagePosition, setPagePosition] = React.useState(1)
+  const [selectedValue, setSelectedValue] = React.useState('');
+
 
   const itemsPerPage = 12
 
   useEffect(() => {
     fetchApi()
+    fetchCombosFromApi()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
@@ -23,7 +32,7 @@ function App() {
 
     const filterCardsSearch = () => {
 
-      if(textSearch.length === 1){
+      if (textSearch.length === 1) {
         setPagePosition(1)
         return
       }
@@ -39,11 +48,22 @@ function App() {
       setFilterCards(cardsPaged)
     }
 
-    
-      filterCardsSearch()
-   
+
+    filterCardsSearch()
+
 
   }, [cards, pagePosition, textSearch])
+
+  useEffect(() => {
+    if (selectedValue !== '') {
+      fetchSeriesApi()
+    }
+    else {
+      fetchApi()
+    }
+    
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedValue])
 
   const fetchApi = async () => {
     const result = await fetch('https://api.tcgdex.net/v2/es/cards')
@@ -70,17 +90,57 @@ function App() {
     //console.log(cardsWithImage)
   }
 
+  const fetchSeriesApi = async () => {
+    const result = await fetch(`https://api.tcgdex.net/v2/es/sets/${selectedValue}`)
+    const data = await result.json()
+
+    const seriesCards = data.cards
+
+    const cardsWithImage = seriesCards.filter((card: any) => {
+      if (card.image) {
+        return card
+      }
+    }
+    ).sort((a: any, b: any) => {
+      if (a.name > b.name) {
+        return 1
+      }
+      if (a.name < b.name) {
+        return -1
+      }
+    })
+
+    setCards(cardsWithImage)
+    const cardsWithImageFiltered = cardsWithImage.splice(0, itemsPerPage * pagePosition)
+    setFilterCards(cardsWithImageFiltered)
+  }
+
+  const fetchCombosFromApi = async () => {
+    const resultSeries = await fetch('https://api.tcgdex.net/v2/es/sets')
+    const dataSeries = await resultSeries.json()
+    const seriesSorted = dataSeries.sort((a: any, b: any) => {
+      if (a.name > b.name) {
+        return 1
+      }
+      if (a.name < b.name) {
+        return -1
+      }
+    })
+    setSeries(seriesSorted)
+    
+  }
+
 
 
   const viewCard = (cardIndex: any) => {
-    
 
-    if(textSearch !== '' && cardIndex < filterCards.length && cardIndex >= 0){
+
+    if (textSearch !== '' && cardIndex < filterCards.length && cardIndex >= 0) {
       filterCards[cardIndex].index = cardIndex
       setCurrentCard(filterCards[cardIndex])
     }
-    
-    if(textSearch === '' &&  cardIndex >= 0){
+
+    if (textSearch === '' && cardIndex >= 0) {
       cards[cardIndex].index = cardIndex
       setCurrentCard(cards[cardIndex])
     }
@@ -88,14 +148,19 @@ function App() {
 
   const handlerErrorImagen = (event: any) => {
 
-    if( event.target.src.toLowerCase().includes('high.webp')){
+    if (event.target.src.toLowerCase().includes('high.webp')) {
       event.target.src = event.target.src.replace('high.webp', 'low.webp')
     }
-    else{
+    else {
       event.target.src = noImage
     }
 
-    
+
+  }
+
+  const handleChange = (event: any) => {
+    setPagePosition(1)
+    setSelectedValue(event.target.value)
   }
 
 
@@ -103,12 +168,26 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Cartas Pokemon de Aarón</h1>
+        
+        <div className='App-filters'>
+          <label className='App-label' htmlFor='cbSeries'>Series</label>
+          <select id='cbSeries' title='Filtro series' value={selectedValue} onChange={handleChange}>
+          <option key='Todas' value='' title='Todas'>Todas</option>
+            {series.map((option: ISeries) => (
+              <option key={option.id} value={option.id} title={option.name}>
+                {option.name}
+              </option>
+            ))}
+          </select>
+        </div>
+
+
         {!currentCard && <input type="text" value={textSearch} className='App-search' placeholder='Buscar' onChange={(event) => setTextSearch(event.target.value)} />}
         <div className='App-Cards-container'>
 
           {!currentCard ?
             <>
-              {filterCards.map((card: any, index:number) => {
+              {filterCards.map((card: any, index: number) => {
                 return <div className='App-name' key={card.id} onClick={() => viewCard(index)}>
                   <div className='App-Pokemon-title' >{card.name}</div>
                   <img className='preview-img' src={`${card.image}/low.webp`} alt={card.name} onError={handlerErrorImagen} />
@@ -121,8 +200,8 @@ function App() {
             <div className='App-card-Image-container'>
               <h3 className='App-return' onClick={() => setCurrentCard(undefined)}>Volver</h3>
               <img className='image-card' src={`${currentCard.image}/high.webp`} alt={currentCard.name} onError={handlerErrorImagen} />
-              <div className='App-pagination' onClick={() => { viewCard(currentCard.index-1) }}>Anterior</div>
-              <div className='App-pagination' onClick={() => { viewCard(currentCard.index+1) }}>Siguiente</div>
+              <div className='App-pagination' onClick={() => { viewCard(currentCard.index - 1) }}>Anterior</div>
+              <div className='App-pagination' onClick={() => { viewCard(currentCard.index + 1) }}>Siguiente</div>
 
             </div>
           }
